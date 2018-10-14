33ac342a7326b3e1efde53254119469c77060d87
cesarsystem04 <cesarsystem04@gmail.com>
2018-10-13 22:58:35 -0500

    Se agrega una case para el modelado de los Objetos.

---
+++ TestNetCore/Modelo/Peliculas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestNetCore.Modelo
+{
+    public class Peliculas
+    {
+        int IdPelicula { set; get; }
+        string strNombre { set; get; }
+    }
+}
